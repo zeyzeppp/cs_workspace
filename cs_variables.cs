ac29f50fd60ed9866b0457417f11f1127b2e27f7
Zeynep Alperen <123326315+zeyzeppp@users.noreply.github.com>
2023-10-21 16:53:56 +0300

    Create cs_variables.cs

---
+++ cs_variables.cs
@@ -0,0 +1,29 @@
+// Zeynep Alperen - 21 Ekim 2023
+
+using System;
+
+namespace cs
+{
+
+    class cs_variables
+    {
+
+        public static void Main2()
+        {
+            string characterName = "Zeynep";
+
+            int characterAge = 20;
+
+            Console.WriteLine("Merhaba benim adım " + characterName);
+
+            Console.WriteLine("ve benim yasım " + characterAge);
+
+            Console.ReadLine();
+
+        }
+
+
+
+
+    }
+}
