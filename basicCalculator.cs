ce55b37d0ca4387ade5c824df79a5dcd94cf2d78
Zeynep Alperen <123326315+zeyzeppp@users.noreply.github.com>
2023-10-21 22:58:58 +0300

    Create basicCalculator.cs

---
+++ basicCalculator.cs
@@ -0,0 +1,30 @@
+// Zeynep Alperen - 21 Ekim 2023
+
+using System;
+
+namespace cs
+{
+
+    class basicCalculator
+    {
+
+        public static void Main()
+        {
+
+
+            Console.Write("Bir sayi giriniz: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Bir sayi giriniz: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Cevap: " + (num1 + num2));
+
+
+
+
+        }
+
+    }
+
+}
