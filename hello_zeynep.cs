59396292dac50ca4a2349753b63a0771ac555d89
Zeynep Alperen <123326315+zeyzeppp@users.noreply.github.com>
2023-10-21 16:52:55 +0300

    Create hello_zeynep.cs

---
+++ hello_zeynep.cs
@@ -0,0 +1,21 @@
+// Zeynep Alperen - 21 Ekim 2023
+
+using System;
+
+namespace cs
+{
+
+    class hello_zeynep
+    {
+
+        public static void Main()
+        {   
+
+            Console.WriteLine("Merhaba Zeynep Alperen");
+
+            Console.ReadLine();
+            
+        }
+    }
+
+}
