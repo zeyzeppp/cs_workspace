d12e430d1309dea0eadad6e74b753bcda488268e
Zeynep Alperen <123326315+zeyzeppp@users.noreply.github.com>
2023-10-21 20:10:49 +0300

    Create playing_with_numbers.cs

---
+++ playing_with_numbers.cs
@@ -0,0 +1,29 @@
+// Zeynep Alperen - 21 Ekim 2023
+
+using System;
+
+namespace cs
+{
+    class playing_with_numbers
+    {
+
+        public static void Main()
+        {
+            int num1 = 25;
+            int num2 = 14;
+
+            Console.WriteLine(num1 + num2);
+
+            Console.WriteLine(Math.Pow(2, 4));
+            Console.WriteLine(Math.Sqrt(49));
+            Console.WriteLine(Math.Abs(-7));
+            Console.WriteLine(Math.Max(21, 20));
+            Console.WriteLine(Math.Min(21, 20));
+            Console.WriteLine(Math.Round(2.8));
+
+
+        }
+
+    }
+
+}
