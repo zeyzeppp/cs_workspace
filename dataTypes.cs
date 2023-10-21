e4e27eb9b5363f396bfb88e3c6953297365e879f
Zeynep Alperen <123326315+zeyzeppp@users.noreply.github.com>
2023-10-21 16:54:46 +0300

    Create dataTypes.cs

---
+++ dataTypes.cs
@@ -0,0 +1,31 @@
+// Zeynep Alperen - 21 Ekim 2023
+
+using System;
+
+
+namespace cs
+{
+
+    class dataTypes
+    {
+
+        public static void Main()
+        {
+            string country = "Turkiye";
+
+            char letter = 'Z';
+
+            int number = 7;
+
+            double gpa = 0.27;
+
+
+            Console.WriteLine("Country: " + country);
+            Console.WriteLine("Letter: " + letter);
+            Console.WriteLine("Number: " + number);
+            Console.WriteLine("GPA: " + gpa);
+
+        }
+    }
+
+}
