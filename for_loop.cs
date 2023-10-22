c8422527f4bd294954f4e842781f63edd6292c2a
Zeynep Alperen <123326315+zeyzeppp@users.noreply.github.com>
2023-10-22 08:27:49 +0300

    Create for_loop.cs

---
+++ for_loop.cs
@@ -0,0 +1,23 @@
+// Zeynep Alperen - 22 Ekim 2023
+
+using System;
+
+namespace cs
+{
+
+    class for_loop
+    {
+        public static void Main()
+        {
+
+            int i;
+
+            for(i = 5; i <= 100; i += 5)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
